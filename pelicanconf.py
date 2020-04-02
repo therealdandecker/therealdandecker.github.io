426d99233c19e9e51a2c50da1e9992edcd457d35
Dan Decker <daniel.decker1@gmail.com>
2020-04-01 23:25:05 -0400

    Revert "dd"
    This reverts commit 281b95d1af6a1c353369317763e97741e3bda17c.

--- pelicanconf.py
+++ pelicanconf.py
@@ -38,7 +38,6 @@ DEFAULT_PAGINATION = 5
 
 #HOME_COVER = 'https://casper.ghost.org/v1.0.0/images/welcome.jpg'
 
-THEME = 'attila'
 
 # Post and Pages path
 ARTICLE_URL = '{date:%Y}/{date:%m}/{slug}.html'
