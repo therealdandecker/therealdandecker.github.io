01cfe22a8a803560ed261d1f7be541003a1a536a
Dan Decker <daniel.decker1@gmail.com>
2020-04-04 00:04:21 -0400

    dd

--- pelicanconf.py
+++ pelicanconf.py
@@ -2,6 +2,13 @@
 # -*- coding: utf-8 -*- #
 from __future__ import unicode_literals
 import webassets
+import jinja2
+from jinja2 import environment
+import jinja2.ext
+from jinja2.ext import loopcontrols
+from jinja2.ext import i18n
+from jinja2.ext import with_
+from jinja2.ext import do
 
 
 AUTHOR = 'Dan'
@@ -32,7 +39,44 @@ LINKS = (('Pelican', 'http://getpelican.com/'),
 SOCIAL = (('LinkedIn', 'https://www.linkedin.com/in/dan-d-789a1ab/'),
           ('GitHub', 'https://github.com/therealdandecker'),)
 
+# Pagination
 DEFAULT_PAGINATION = 5
+PAGINATION_PATTERNS = (
+    (1, '{base_name}/', '{base_name}/index.html'),
+    (2, '{base_name}/page/{number}/', '{base_name}/page/{number}/index.html'),
+)
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
+
+STATIC_PATHS = ['assets']
+
+PLUGIN_PATHS = [
+  'pelican-plugins'
+]
+
+PLUGINS = [
+  'sitemap',
+  'neighbors',
+  'assets'
+]
+
+# Sitemap
+SITEMAP = {
+    'format': 'xml',
+    'priorities': {
+        'articles': 0.5,
+        'indexes': 0.5,
+        'pages': 0.5
+    },
+    'changefreqs': {
+        'articles': 'monthly',
+        'indexes': 'daily',
+        'pages': 'monthly'
+    }
+}
+
+
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
