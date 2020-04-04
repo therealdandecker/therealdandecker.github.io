c8af223e0bc8adae79dcc06ab08a0bb65543387a
Dan Decker <daniel.decker1@gmail.com>
2020-04-04 00:09:06 -0400

    dd

---
+++ pelicanconf.py
@@ -0,0 +1,131 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+import webassets
+import jinja2
+from jinja2 import environment
+import jinja2.ext
+from jinja2.ext import loopcontrols
+from jinja2.ext import i18n
+from jinja2.ext import with_
+from jinja2.ext import do
+
+
+AUTHOR = 'Dan'
+SITENAME = 'Dan Decker'
+SITESUBTITLE = 'A semi-pro blog'
+SITEURL = 'https://dandecker.us'
+
+PATH = 'content'
+
+TIMEZONE = 'America/New_York'
+
+DEFAULT_LANG = 'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (('Pelican', 'http://getpelican.com/'),
+         ('Python.org', 'http://python.org/'),
+         ('Jinja2', 'http://jinja.pocoo.org/'),
+         ('Anaconda Distribution', 'https://www.anaconda.com/distribution/'),)
+
+# Social widget
+SOCIAL = (('LinkedIn', 'https://www.linkedin.com/in/dan-d-789a1ab/'),
+          ('GitHub', 'https://github.com/therealdandecker'),)
+
+# Pagination
+DEFAULT_PAGINATION = 5
+PAGINATION_PATTERNS = (
+    (1, '{base_name}/', '{base_name}/index.html'),
+    (2, '{base_name}/page/{number}/', '{base_name}/page/{number}/index.html'),
+)
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
+
+STATIC_PATHS = ['assets']
+
+PLUGIN_PATHS = [
+  'pelican-plugins'
+]
+
+PLUGINS = [
+  'sitemap',
+  'neighbors',
+  'assets'
+]
+
+# Sitemap
+SITEMAP = {
+    'format': 'xml',
+    'priorities': {
+        'articles': 0.5,
+        'indexes': 0.5,
+        'pages': 0.5
+    },
+    'changefreqs': {
+        'articles': 'monthly',
+        'indexes': 'daily',
+        'pages': 'monthly'
+    }
+}
+
+
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
+
+#HOME_COVER = 'https://casper.ghost.org/v1.0.0/images/welcome.jpg'
+
+THEME ="attila"
+
+# Post and Pages path
+ARTICLE_URL = '{date:%Y}/{date:%m}/{slug}.html'
+ARTICLE_SAVE_AS = '{date:%Y}/{date:%m}/{slug}.html'
+PAGE_URL = 'pages/{slug}/'
+PAGE_SAVE_AS = 'pages/{slug}/index.html'
+YEAR_ARCHIVE_SAVE_AS = '{date:%Y}/index.html'
+MONTH_ARCHIVE_SAVE_AS = '{date:%Y}/{date:%m}/index.html'
+
+# Tags and Category path
+CATEGORY_URL = 'category/{slug}'
+CATEGORY_SAVE_AS = 'category/{slug}/index.html'
+CATEGORIES_SAVE_AS = 'catgegories.html'
+TAG_URL = 'tag/{slug}'
+TAG_SAVE_AS = 'tag/{slug}/index.html'
+TAGS_SAVE_AS = 'tags.html'
+
+# Author
+AUTHOR_URL = 'author/{slug}'
+AUTHOR_SAVE_AS = 'author/{slug}/index.html'
+AUTHORS_SAVE_AS = 'authors.html'
+
+AUTHORS_BIO = {
+  "Dan": {
+    "name": "Dan Decker",
+    #"cover": "https://casper.ghost.org/v1.0.0/images/team.jpg",
+    #"image": "assets/images/avatar.png",
+    "website": "https://dandecker.us",
+    "linkedin": "https://www.linkedin.com/in/dan-d-789a1ab/",
+    "github": "https://github.com/therealdandecker",
+    "location": "USA",
+    "bio": "Dan Decker is not a pulizer winning writer, but anyone wiht Python skills can make a website these days."
+  }
+}
+
+JINJA_ENVIRONMENT = {
+  'extensions' :[
+    'jinja2.ext.loopcontrols',
+    'jinja2.ext.i18n',
+    'jinja2.ext.with_',
+    'jinja2.ext.do'
+  ]
+}
+
+JINJA_FILTERS = {'max': max}
